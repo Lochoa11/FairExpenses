b4b698cca673614bd4c628430056bbcc0b246f4d
Lochoa11 <linochoa11690@gmail.com>
2025-04-22 17:13:36 -0400

    Initial Commit

---
+++ FairExpenses/FairExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairExpensesApp.swift
+//  FairExpenses
+//
+//  Created by Lin Ochoa on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FairExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
